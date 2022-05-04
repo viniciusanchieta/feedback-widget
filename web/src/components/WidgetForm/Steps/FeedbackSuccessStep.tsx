bbb6aded95716f678c138fd4ce6b01140bae6e99
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-05-04 02:13:30 -0300

    feature: Create form for sent of feedbacks

--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -26,7 +26,7 @@ export function FeedbackSuccessStep({onFeedbackRestartRequested}: FeedbackSucces
                 className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
                 >
                     Quero enviar outro
-                </button>
+                </button> 
             </div>
         </>
     )
