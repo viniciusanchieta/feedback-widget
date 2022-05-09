d592dded285c9afc18aee264811fcbcf3d3b7b4f
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-05-09 01:06:22 -0300

    feat: Mobile project finished

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,34 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Widget from './src/components/Widget';
+import AppLoading from 'expo-app-loading';
+import { useFonts, Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+
+import { theme } from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return < AppLoading />;
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <StatusBar 
+      style="light" 
+      backgroundColor="transparent"
+      translucent
+      />
+
+      <Widget />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mobile/src/components/Options/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Copyright } from '../Copyright';
+
+import { styles } from './styles';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+import { Option } from '../Option';
+import { FeedbackType } from '../Widget';
+
+interface Props {
+  onFeedbackTypeChanged: (FeedbackType: FeedbackType) => void;
+}
+
+export function Options({ onFeedbackTypeChanged }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Deixe seu feedback
+      </Text>
+      <View style={styles.options}>
+        {
+          Object
+            .entries(feedbackTypes)
+            .map(([key, value]) => (
+              <Option
+                key={key}
+                title={value.title}
+                image={value.image}
+                onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
+              />
+            ))
+        }
+      </View>
+      <Copyright />
+    </View>
+  );
+}
