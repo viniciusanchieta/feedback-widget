1e01074dc75acde646f19ef248ca01aa82179736
Vinicius Anchieta <vinicius.anchieta@outlook.com>
2022-05-10 23:37:03 -0300

    feat: Set limit of large file and add image in email

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,7 +5,8 @@ import { routes } from './routes';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.use(routes);
 
--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -39,7 +39,7 @@ export class SubmitFeedbackUseCase{
                 `<div style="font-family: sans-serif; font-size:16px; color: #111" >`,
                 `<p>Tipo do feedback: ${type}</p>`,
                 `<p>Comentário: ${comment}</p>`,
-                `<p><img src="${screenshot}" width:"100%"/></p>`,
+                screenshot ? `<img src="${screenshot}"/>`: ``,
                 `</div>`
             ].join('\n')
         })
